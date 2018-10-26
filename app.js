d1d89ea5509a37467de12add15925d8b1fcf51e4
chips <jcaximenez@gmail.com>
2018-10-25 21:58:13 -0500

    package.json start y engines

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Home');
@@ -13,4 +14,4 @@ app.get('/contact', (req, res) => {
 	res.send('Contact');
 })
 
-app.listen(3000);
+app.listen(port);
